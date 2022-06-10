e25e686f5b3210ed2325d11d35b5a4dad7737b74
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-10 22:52:53 +0900

    액티비티 기본 구조 설정

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/MainActivity.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/MainActivity.kt
@@ -13,5 +13,15 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
