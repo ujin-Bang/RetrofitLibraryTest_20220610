6cbdad138aec93205f96a50b4b002b688595b640
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-11 10:43:47 +0900

    통신 담당 객체 (Retrofit)얻어오는 함수 추가

---
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/api/ServerAPI.kt
@@ -0,0 +1,33 @@
+package com.start.retrofitlibrarytest_20220610.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ServerAPI {
+
+    // Retrofit 타입 객체 하나만 생성 -> 모두가 공유 (싱글턴 패턴)
+
+    companion object {
+
+//        기본 접속 서버
+        private var BASE_URL = "http://3.34.159.73"
+
+        private var retrofit : Retrofit? = null //앱이 처음 켜질때는 없다. => 한번만 만들고 함수를 통해서 공유.
+
+        fun getRetrofit() : Retrofit {
+
+            if(retrofit == null) {
+//                통신 담당 객체를 만들지 않았다면 => 없을때만 새로 한번만들자.
+
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL) //어느 서버로 접속?
+                    .addConverterFactory(GsonConverterFactory.create()) //파싱을 자동 도구로 활용.
+                    .build()
+            }
+
+            return retrofit!!
+
+        }
+    }
+
+}
