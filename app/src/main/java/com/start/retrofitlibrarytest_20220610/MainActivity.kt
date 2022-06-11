6073ae18bea50452f47eb55274d932b885bafc1a
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-11 12:07:43 +0900

    로그인 응답 처리  - 자동 차싱된 BasicResponse 변수 이용

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/MainActivity.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/MainActivity.kt
@@ -3,8 +3,13 @@ package com.start.retrofitlibrarytest_20220610
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.start.retrofitlibrarytest_20220610.databinding.ActivityMainBinding
+import com.start.retrofitlibrarytest_20220610.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -24,7 +29,22 @@ class MainActivity : BaseActivity() {
             val inputEamil = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
 
-            apiService.postRequestLogin(inputEamil, inputPw)
+            apiService.postRequestLogin(inputEamil, inputPw).enqueue( object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    val basicResponse = response.body()!!
+
+                    Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
         }
 
     }
