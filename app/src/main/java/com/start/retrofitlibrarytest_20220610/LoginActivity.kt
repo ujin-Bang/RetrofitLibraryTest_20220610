123074f8f57bb00011ab146f83cc8a33a11aba75
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-12 14:11:51 +0900

    브랜치 테스트용 주석

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/LoginActivity.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/LoginActivity.kt
@@ -11,7 +11,7 @@ import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
+//로그인 화면 - 방우진이 작업합니다.
 class LoginActivity : BaseActivity() {
 
     lateinit var binding: ActivityLoginBinding
