87460d131dcf7e0a53e9490fecf85fbbd1883987
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 18:13:46 +0900

    내프로필 프래그먼트 -> 내 정보 받아오기 실행   - Glide 라이브러리 추가 설치   - 토큰 직접 추가 : 임시 방편임.

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/MyProfileFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.start.retrofitlibrarytest_20220610.R
 import com.start.retrofitlibrarytest_20220610.databinding.FragmentMyProfileBinding
+import com.start.retrofitlibrarytest_20220610.datas.BasicResponse
+import com.start.retrofitlibrarytest_20220610.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileFragment: BaseFragment() {
 
@@ -33,5 +39,33 @@ class MyProfileFragment: BaseFragment() {
 
     override fun setValues() {
 
+    getMyInfoFromServer()}
+
+
+
+
+    fun getMyInfoFromServer() {
+
+        apiService.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object :
+            Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    val br = response.body()!!
+
+                    binding.txtNickname.text = br.data.user.nickname
+                    Glide.with(mContext).load(br.data.user.profileImageURL).into(binding.imgProfile)
+
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
     }
 }
