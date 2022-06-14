722a3979ceff0900f8f0cac7385dc929eeb38fad
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 17:56:30 +0900

    BaseFragment 클래스 추가

---
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/BaseFragment.kt
@@ -0,0 +1,21 @@
+package com.start.retrofitlibrarytest_20220610.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    lateinit var mContext: Context
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mContext = requireContext()
+
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
