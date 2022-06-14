c5a014f3ad59f3da6ec409a4d0d9bb2253cf62ce
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 21:11:52 +0900

    어댑터 - 실제 데이터(상품이름, 상품이미지)만 반영

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/adpaters/ProductAdapter.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/adpaters/ProductAdapter.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.createBitmap
+import com.bumptech.glide.Glide
 import com.start.retrofitlibrarytest_20220610.R
 import com.start.retrofitlibrarytest_20220610.datas.ProductData
 
@@ -20,6 +24,18 @@ class ProductAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgStoreLogo = row.findViewById<ImageView>(R.id.imgStoreLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+        val txtProductName = row.findViewById<TextView>(R.id.txtProductName)
+        val txtCategoryName = row.findViewById<TextView>(R.id.txtCategoryName)
+
+        Glide.with(mContext).load(data.image_url).into(imgStoreLogo)
+        txtProductName.text = data.name
+
+
         return row
     }
 }
--- app/src/main/java/com/start/retrofitlibrarytest_20220610/datas/ProductData.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/datas/ProductData.kt
@@ -3,5 +3,9 @@ package com.start.retrofitlibrarytest_20220610.datas
 class ProductData(
     var id: Int,
     var name: String,
+    var price: Int,
+    var image_url: String,
+    var store_id: Int,
+
 ) {
 }
