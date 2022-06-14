816a71666b2193ebe19c413c4bc09d4dd7f6ae03
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 20:14:16 +0900

    products JSONArray 파싱  - 내부 데이터는 ProductData를 활용

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/datas/DataResponse.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/datas/DataResponse.kt
@@ -3,7 +3,9 @@ package com.start.retrofitlibrarytest_20220610.datas
 class DataResponse(
     var user: UserData,
     var token: String,
-    var products: ProductData,
+
+//    이 변수는 상품목록에서만 사용
+    var products: List<ProductData>,
 
 ) {
 }
