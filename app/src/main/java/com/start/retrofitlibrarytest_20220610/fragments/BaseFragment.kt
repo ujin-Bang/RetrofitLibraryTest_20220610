b6c74f36c4a98e0e3b0139f31a829cd1b1fa5a5f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 18:01:21 +0900

    프래그먼트들도 서버 호출 apiService 상속

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/BaseFragment.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/BaseFragment.kt
@@ -3,16 +3,23 @@ package com.start.retrofitlibrarytest_20220610.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.start.retrofitlibrarytest_20220610.api.ServerAPI
+import com.start.retrofitlibrarytest_20220610.api.ServerAPIService
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
+
+    //    상속 시켜 줄것 : API 호출 명세를 담은 변수
+    lateinit var apiService : ServerAPIService
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         mContext = requireContext()
 
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
