efe7f89c5c27c7c39f0e3fd19424fa46799e75dc
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 17:39:13 +0900

    토큰 받아서 저장하는 기능 추가

---
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/utils/ContextUtil.kt
@@ -0,0 +1,25 @@
+package com.start.retrofitlibrarytest_20220610.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        //        어떤 파일? 파일 명
+        private val prefName = "RetrofitTestPref"
+
+        //        어떤 데이터 항목?  항목 명
+        private val TOKEN = "TOKEN"
+
+//        저장 함수 (setter) / 조회 함수 (getter)
+
+        fun setToken(context: Context, token: String) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(TOKEN, token).apply()
+
+        }
+
+    }
+}
