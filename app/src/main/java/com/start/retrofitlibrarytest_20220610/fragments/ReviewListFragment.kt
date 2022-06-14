3cd709c81ffcc13e2ce179d391e99df8e3ce5756
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-14 15:59:50 +0900

    프래그먼트에서의 데이터바인딩

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/ReviewListFragment.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/ReviewListFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.start.retrofitlibrarytest_20220610.R
+import com.start.retrofitlibrarytest_20220610.databinding.FragmentReviewListBinding
 
 class ReviewListFragment : Fragment() {
+
+    lateinit var binding: FragmentReviewListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_review_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_review_list, container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
