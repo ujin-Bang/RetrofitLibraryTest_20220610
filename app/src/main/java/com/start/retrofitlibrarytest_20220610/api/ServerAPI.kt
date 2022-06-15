5c5c76f568907132d6a4310459f5382a5db4fb0e
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-15 16:47:10 +0900

    모든 API가 자동으로 헤더를 첨부하게  - Intercepter개념 활용

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/api/ServerAPI.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/api/ServerAPI.kt
@@ -1,5 +1,9 @@
 package com.start.retrofitlibrarytest_20220610.api
 
+import android.content.Context
+import com.start.retrofitlibrarytest_20220610.utils.ContextUtil
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -15,15 +19,43 @@ class ServerAPI {
 
         private var retrofit : Retrofit? = null //앱이 처음 켜질때는 없다. => 한번만 만들고 함수를 통해서 공유.
 
-        fun getRetrofit() : Retrofit {
+        fun getRetrofit(context: Context) : Retrofit {
 
             if(retrofit == null) {
 
 //                통신 담당 객체를 만들지 않았다면 => 없을때만 새로 한번만들자.
 
+//                retrofit 변수에 추가 설정 진행.
+
+//                1. 토큰 자동 첨부(retrofit 변수를 더 가공)
+
+//                API요청이 들어오면 => 가로채서, 헤더를 추가해주자. => 그 뒤로 나머지 API요청 진행.
+
+                val interceptor = Interceptor{
+
+                    with(it){
+
+//                        헤더를 추가한 새 리퀘스트 생성
+                        val newRequest = request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getToken(context))
+                            .build()
+
+//                        새 리궤스트로 나머지 동작 이어가기.
+                        proceed(newRequest)
+                    }
+
+                }
+
+//                Retrofit은 OkHttp의 확장판 => 클라이언트의 역할 : OkHttpClient 클래스 활용
+//                커스텀 클라이언트 제작.(API요청에 헤더를 붙이는)
+                val myClient = OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL) //어느 서버로 접속?
                     .addConverterFactory(GsonConverterFactory.create()) //파싱을 자동 도구로 활용.
+                    .client(myClient)
                     .build()
             }
 
