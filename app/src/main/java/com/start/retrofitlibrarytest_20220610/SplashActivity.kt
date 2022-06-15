a5be1d0787c119696609d3ad6fb5a97e9ade8e3e
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-15 12:06:08 +0900

    1.5초 후에 자동 로그인

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/SplashActivity.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.start.retrofitlibrarytest_20220610
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil.setContentView
+import com.start.retrofitlibrarytest_20220610.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +22,19 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+          val myIntent: Intent
+
+          if(ContextUtil.getToken(mContext) != null) {
+              myIntent = Intent(mContext, MainActivity::class.java)
+          }
+          else{
+              myIntent = Intent(mContext,LoginActivity::class.java)
+          }
+            startActivity(myIntent)
+            finish()
+        },1500)
     }
 }
