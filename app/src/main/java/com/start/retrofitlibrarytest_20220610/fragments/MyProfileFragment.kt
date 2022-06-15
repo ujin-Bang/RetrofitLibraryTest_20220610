d39b28941b749b96b40ffae64aaf3fc0c6c4dfc0
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-15 18:26:21 +0900

    닉네임 변경 기능 구현예시 (3)  - 변경 성공시 사용자 정보 갱신(GlobalData)

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/fragments/MyProfileFragment.kt
@@ -76,6 +76,9 @@ class MyProfileFragment : BaseFragment() {
 
                             ContextUtil.setToken(mContext, token)
 
+//                            로그인한 사용자 정보도 갱신.
+                            GlobalData.loginUser = br.data.user
+
                             Toast.makeText(mContext, "닉네임 변경에 성공했습니다.", Toast.LENGTH_SHORT).show()
 
                             getMyInfoFromServer()
