f64fea39d06b4e4abc62bb9363ffc388bfd0eec8
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-18 11:40:42 +0900

    onCreateViewHolder  - xml 끌어오기 -> viwType에 따라 다르게  - ViewHolder 클래스도 각각 다르게

--- app/src/main/java/com/start/retrofitlibrarytest_20220610/adpaters/MainRecylerAdapter.kt
+++ app/src/main/java/com/start/retrofitlibrarytest_20220610/adpaters/MainRecylerAdapter.kt
@@ -1,9 +1,11 @@
 package com.start.retrofitlibrarytest_20220610.adpaters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.start.retrofitlibrarytest_20220610.R
 import com.start.retrofitlibrarytest_20220610.datas.ReviewData
 
 class MainRecylerAdapter(val mContext:Context, val mList: List<ReviewData>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -20,21 +22,35 @@ class MainRecylerAdapter(val mContext:Context, val mList: List<ReviewData>): Rec
 
 //    position별로 어떤 모양이 나가야 하는지(viewType이 어떻게 되는지) 알려줄 함수
 
-    val HEADER_VIEW_TIPE = 1000
+    val HEADER_VIEW_TYPE = 1000
     val REVIEW_ITEM_TYPE = 1001
     override fun getItemViewType(position: Int): Int {
 
-//        position: 맨 윗 칸 => 상단 뷰
+//        position 0: 맨 윗 칸 => 상단 뷰
 //        position 그 외: 목록 표시. => 리뷰 아이템
 
         return when(position){
 
-            0 -> HEADER_VIEW_TIPE
+            0 -> HEADER_VIEW_TYPE
             else -> REVIEW_ITEM_TYPE
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        return when(viewType) {
+
+            HEADER_VIEW_TYPE ->{
+                val row = LayoutInflater.from(mContext).inflate(R.layout.main_recycler_item_top_view, parent, false)
+                HeaderViewHolder(row)
+            }
+            else -> {
+//                리뷰 아이템
+
+                val row = LayoutInflater.from(mContext).inflate(R.layout.main_recycler_item_review_item, parent, false)
+                ItemViewHolder(row)
+            }
+        }
 
     }
 
